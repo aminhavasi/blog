7c4f9f43fdfeead9b334de08e2a3a29165ce1be2
amin havasi <pau.ahq@gmail.com>
2020-12-30 14:10:27 +0330

    logout route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./../models/user');
+const Token = require('./../models/token');
 const { registerValidator, loginValidator } = require('./../validator/auth');
 const { date } = require('./../utils/moment');
 const { hash, deHash } = require('./../functions/hash');
@@ -27,7 +28,6 @@ router.post('/register', async (req, res) => {
             .header('x-auth-token', tokenRes)
             .send('user successfully added');
     } catch (err) {
-        console.log(err);
         res.status(400).send('something went wrong');
     }
 });
@@ -49,8 +49,19 @@ router.post('/login', async (req, res) => {
 
         await res.status(200).header('x-auth-token', tokenRes).send('ok');
     } catch (error) {
-        console.log(error);
         res.status(400).send('bad');
+    }
+});
+
+router.delete('/logout', async (req, res) => {
+    try {
+        const tokenRemove = await Token.findOneAndDelete({
+            token: req.body.token,
+        });
+        if (!tokenRemove) throw errorHandler('token is invalid!');
+        res.status(200).send('logout successfully');
+    } catch (err) {
+        res.status(400).send('faild logout!');
     }
 });
 
