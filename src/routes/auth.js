6307ccfef7b88d9d26d0827c5780e21159214274
amin havasi <pau.ahq@gmail.com>
2021-01-09 19:37:48 +0330

    auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -18,6 +18,7 @@ router.post('/register', async (req, res) => {
     try {
         const { error } = await registerValidator(req.body);
         if (error) return res.status(400).send(error.details[0].message);
+
         const user = await User.findOne({ email: req.body.email });
         if (user) return res.status(400).send('this user Already exict!');
         req.body.date = date;
